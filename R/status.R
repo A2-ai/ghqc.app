975a5f713ebf99687b9fb12b26dd66d19fb7784a
Jenna Johnson <jenna@a2-ai.com>
2025-03-25 16:36:11 +0000

    add diagnostics for qc status and repo files

--- R/status.R
+++ R/status.R
@@ -8,65 +8,100 @@
 #' @export
 #'
 #' @import dplyr purrr
-ghqc_status <- function(owner, repo, milestone_name) {
+ghqc_status <- function(owner, repo, milestone_names) {
   # TODO: have some functionality to gray out Issues in the Milestone that were
   # initialized on a different branch than the local user's branch
 
   git_fetch(prune = TRUE)
+  root_dir <- rproj_root_dir()
   creds <- check_github_credentials()
   token <- creds$token
   remote_name <- creds$remote$name
 
-  issues <- get_all_issues_in_milestone(owner, repo, milestone_name)
+  local_log_output <- system("git log --pretty=format:'%H|%an|%ae|%ad|%s'  --date=format:'%Y-%m-%d %H:%M:%S'", intern = TRUE)
+  local_commit_log <- read.csv(text = local_log_output, sep = "|", header = FALSE, stringsAsFactors = FALSE)
+  names(local_commit_log) <- c("commit", "author_name", "author_email", "time", "message")
 
-  issues_df <- map_df(issues, function(issue) {
+  milestone_dfs <- sapply(milestone_names, function(milestone_name) {
+    issues <- get_all_issues_in_milestone(owner, repo, milestone_name)
 
-    issue_number <- issue$number
-    branch <- get_branch_from_metadata(owner, repo, issue_number)
-    #local_commit_log <- gert::git_log(branch, max = 9999)
+    issues_df <- map_df(issues, function(issue) {
+      issue_number <- issue$number
+      # branch from metadata might be different from current branch
+      branch <- get_branch_from_metadata(owner, repo, issue_number)
 
-    local_log_output <- system("git log --pretty=format:'%H|%an|%ae|%ad|%s'  --date=format:'%Y-%m-%d %H:%M:%S'", intern = TRUE)
-    local_commit_log <- read.csv(text = local_log_output, sep = "|", header = FALSE, stringsAsFactors = FALSE)
-    names(local_commit_log) <- c("commit", "author_name", "author_email", "time", "message")
+      remote_log_output <- system(glue::glue("git log {remote_name}/{branch} --pretty=format:'%H|%an|%ae|%ad|%s'  --date=format:'%Y-%m-%d %H:%M:%S'"), , intern = TRUE)
+      remote_commit_log <- read.csv(text = remote_log_output, sep = "|", header = FALSE, stringsAsFactors = FALSE)
+      names(remote_commit_log) <- c("commit", "author_name", "author_email", "time", "message")
 
-    #remote_commit_log <- gert::git_log(glue::glue("{remote_name}/{branch}"), max = 9999)
+      # latest_qc_commit is the most recent commented commit in file's issue
+      latest_qc_commit <- get_latest_qc_commit(owner, repo, issue_number, token)
+
+      # get column values for file
+      file_name <- issue$title
+      url <- issue$html_url
+      issue_state <- issue$state
+      git_status <- get_file_git_status(file_name,
+                                        local_commits = local_commit_log$commit,
+                                        remote_commits = remote_commit_log$commit)
+
+      qc_status_info <- get_file_qc_status(file = file_name,
+                                           issue_state = issue_state,
+                                           git_status = git_status,
+                                           local_commit_log = local_commit_log,
+                                           remote_commit_log = remote_commit_log,
+                                           latest_qc_commit = latest_qc_commit,
+                                           issue_closed_at = issue$closed_at)
+      qc_status <- qc_status_info$qc_status
+      diagnostics <- qc_status_info$diagnostics
+      qcer <- issue$assignee$login
+
+      tibble(
+        milestone_name = milestone_name,
+        file_name = file_name,
+        url = url,
+        issue_state = issue_state,
+        qc_status = qc_status,
+        git_status = git_status,
+        qcer = qcer,
+        diagnostics = diagnostics
+      )
+    })
+  }) # milestone_dfs
+
+  all_milestones_df <- dplyr::bind_rows(milestone_dfs)
+
+  # add rest of repo files, determine whether they're assoc files or not
+  files_with_issues <- unique(all_milestones_df$file_name)
+  files_in_repo <- list.files(path = root_dir, recursive = TRUE)
+  files_without_issues <- files_in_repo[!files_in_repo %in% files_with_issues]
+
+  repo_files_df <- map_df(files_without_issues, function(file) {
+    # get current branch
+    branch <- gert::git_branch()
     remote_log_output <- system(glue::glue("git log {remote_name}/{branch} --pretty=format:'%H|%an|%ae|%ad|%s'  --date=format:'%Y-%m-%d %H:%M:%S'"), , intern = TRUE)
     remote_commit_log <- read.csv(text = remote_log_output, sep = "|", header = FALSE, stringsAsFactors = FALSE)
     names(remote_commit_log) <- c("commit", "author_name", "author_email", "time", "message")
 
-    # latest_qc_commit is the most recent commented commit in file's issue
-    latest_qc_commit <- get_latest_qc_commit(owner, repo, issue_number, token)
-
-    # get column values for file
-    file_name <- issue$title
-    url <- issue$html_url
-    issue_state <- issue$state
-    git_status <- get_file_git_status(file_name,
+    git_status <- get_file_git_status(file,
                                       local_commits = local_commit_log$commit,
                                       remote_commits = remote_commit_log$commit)
 
-    qc_status <- get_file_qc_status(file = file_name,
-                                    issue_state = issue_state,
-                                    git_status = git_status,
-                                    local_commit_log = local_commit_log,
-                                    remote_commit_log = remote_commit_log,
-                                    latest_qc_commit = latest_qc_commit,
-                                    issue_closed_at = issue$closed_at)
-    qcer <- issue$assignee$login
-
     tibble(
-      milestone_name = milestone_name,
-      file_name = file_name,
-      url = url,
-      issue_state = issue_state,
+      milestone_name = "NA",
+      file_name = file,
+      url = NA,
+      issue_state = "no Issue",
+      qc_status = "NA",
       git_status = git_status,
-      qc_status = qc_status,
-      qcer = qcer,
+      qcer = "NA",
+      diagnostics = "NA"
     )
   })
-  # TODO: add rest of repo files, determine whether they're assoc files or not
 
-  return(issues_df)
+  all_files_df <- dplyr::bind_rows(all_milestones_df, repo_files_df)
+
+  return(all_files_df)
 }
 
 file_has_uncommitted_local_changes <- function(file) {
@@ -217,53 +252,78 @@ get_file_qc_status <- function(file,
   local_commits <- local_commit_log$commit
   remote_commits <- remote_commit_log$commit
 
+  latest_qc_commit_short <- substr(latest_qc_commit, 1, 7)
+
   ## For open issues
   if (issue_state == "open") {
     # if local commit is older than the latest_qc_commit (even if it didn't change the file)
     # if the file has changed remotely and the latest_qc_commit isn't in the local git log history, then QC update to pull
     if (git_status == "remote file changes" && !latest_qc_commit %in% local_commits) {
-      return("QC update to pull")
+      local_commit_short <- local_commits[1]
+      return(list(qc_status = "QC update to pull",
+                  diagnostics = glue::glue("current qc commit: {latest_qc_commit_short}, local commit: {local_commit_short}")
+                  ))
     }
 
-    last_commit_that_changed_file <- last_commit_that_changed_file_after_latest_qc_commit(file,
+    last_remote_commit_that_changed_file <- last_commit_that_changed_file_after_latest_qc_commit(file,
                                                                                             latest_qc_commit,
                                                                                             commit_log = remote_commit_log)$last_commit_that_changed_file
-    if (!is.null(last_commit_that_changed_file)) {
-      return("QC update to comment")
+    if (!is.null(last_remote_commit_that_changed_file)) {
+      last_commit_that_changed_file_short <- substr(last_remote_commit_that_changed_file, 1, 7)
+      return(list(qc_status = "QC update to comment",
+                  diagnostics = glue::glue("current qc commit: {latest_qc_commit_short}, most recent remote file change in commit: {last_commit_that_changed_file_short}")
+                  ))
     }
 
-    return("QC in progress")
+    return(list(qc_status = "QC in progress",
+                diagnostics = "NA"
+                ))
   } # open
 
   ## For closed issues
   else if (issue_state == "closed") {
     if (git_status == "local uncommitted file changes") {
-      return("local uncommitted file changes after Issue closure")
+      return(list(qc_status = "local uncommitted file changes after Issue closure",
+                  diagnostics = glue::glue("final qc commit: {latest_qc_commit_short}, local uncommitted file changes after final qc commit")
+                  ))
     }
 
     if (git_status == "local unpushed commits with file changes") {
-      return("local unpushed commits with file changes with file changes after Issue closure")
+      last_local_commit_that_changed_file <- last_commit_that_changed_file_after_latest_qc_commit(file,
+                                                                                            latest_qc_commit,
+                                                                                            commit_log = local_commit_log)$last_commit_that_changed_file
+      last_local_commit_that_changed_file_short <-  substr(last_local_commit_that_changed_file, 1, 7)
+
+      return(list(qc_status = "local unpushed commits with file changes with file changes after Issue closure",
+                  diagnostics = glue::glue("final qc commit: {latest_qc_commit_short}, most recent local file change in commit: {last_local_commit_that_changed_file_short}")
+                                           ))
     }
 
     # if there exists a commit that changed the file after the latest qc commit,
     file_change_info <- last_commit_that_changed_file_after_latest_qc_commit(file,
                                                                               latest_qc_commit,
                                                                               commit_log = remote_commit_log)
-    last_commit_that_changed_file <- file_change_info$last_commit_that_changed_file
+    last_remote_commit_that_changed_file <- file_change_info$last_commit_that_changed_file
 
-    if (!is.null(last_commit_that_changed_file)) {
+    if (!is.null(last_remote_commit_that_changed_file)) {
       # was the commit before or after Issue closure?
       commit_time <- as.POSIXct(file_change_info$commit_time)
       issue_close_time <- as.POSIXct(issue_closed_at, format = "%Y-%m-%dT%H:%M:%SZ", tz = "UTC")
 
-      if (commit_time < issue_close_time) {
-        return("uncommented pushed file changes before Issue closure")
-      }
+      last_commit_that_changed_file_short <- substr(last_remote_commit_that_changed_file, 1, 7)
 
-      return("pushed file changes after Issue closure")
+      if (commit_time < issue_close_time) {
+        return(list(qc_status = "uncommented pushed file changes before Issue closure",
+                    diagnostics = glue::glue("final qc commit: {latest_qc_commit_short}, most recent file change in commit: {last_commit_that_changed_file_short}")
+                    ))
+      }
+      return(list(qc_status = "pushed file changes after Issue closure",
+             diagnostics = glue::glue("final qc commit: {latest_qc_commit_short}, most recent file change in commit: {last_commit_that_changed_file_short}")
+             ))
     } # if file changed
 
-   return("QC Complete")
+   return(list(qc_status = "QC Complete",
+               diagnostics = "NA"))
   } # closed
 
   ## For non-issue files
@@ -271,7 +331,9 @@ get_file_qc_status <- function(file,
     # TODO: Associated file
 
     # NA
-    return("NA")
+    return(list(qc_status = "NA",
+                diagnostics = "NA"
+                ))
   }
 
   else {
