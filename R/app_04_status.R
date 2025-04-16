6673119cccbf96bbb205a06a661129bafedf2cf7
jenna-a2ai <jenna@a2-ai.com>
2025-04-16 20:48:21 +0000

    add error handling if no ghqc milestones

--- R/app_04_status.R
+++ R/app_04_status.R
@@ -10,6 +10,8 @@
 #' @importFrom log4r info debug warn error
 #' @export
 ghqc_status_app <- function(milestones = NULL) {
+  inputted_milestones <- milestones
+
   if (!exists("config_repo_path", .le)) ghqc_set_config_repo()
   get_options()
   git_fetch(prune = TRUE)
@@ -23,16 +25,23 @@ ghqc_status_app <- function(milestones = NULL) {
   repo <- get_repo_errors(remote)
 
   all_ghqc_milestones <- list_ghqc_milestones(org, repo)
+
+  # error if no ghqc milestones
+  if (length(all_ghqc_milestones) == 0) {
+    error(.le$logger, glue::glue("There were no ghqc Milestones found in {org}/{repo}. Create ghqc Milestones using `ghqc_assign_app()`"))
+    rlang::abort("There were no open Milestones found.")
+  }
+
   all_ghqc_milestone_names <- purrr::map_chr(all_ghqc_milestones, "title")
 
-  all_inputted_milestones_valid <- all(milestones %in% all_ghqc_milestone_names)
+  all_inputted_milestones_valid <- all(inputted_milestones %in% all_ghqc_milestone_names)
   if (!(all_inputted_milestones_valid)) {
     info(.le$logger, "Not all inputted Milestones exist. Rendering table with most recent Milestone")
   }
 
   default_milestones <- {
-    if (!is.null(milestones) && all_inputted_milestones_valid) {
-    milestones
+    if (!is.null(inputted_milestones) && all_inputted_milestones_valid) {
+      inputted_milestones
     } else {
       get_most_recent_milestone(all_ghqc_milestones)
     }
