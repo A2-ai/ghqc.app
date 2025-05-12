ab5bb691e7ee254ed41540a2491e92198302b8d8
Jenna Johnson <jenna@a2-ai.com>
2025-05-12 15:39:47 +0000

    stubs

---
+++ R/archive.R
@@ -0,0 +1,6 @@
+# structure of issue objects should be like those outputted from ghqc_status
+# named by milestone and issue title
+
+ghqc_archive <- function(issue_objects, output_directory) {
+
+}
